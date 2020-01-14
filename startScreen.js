a2c5cf2ceeb051a5d4065ea743c6d1b3d1bb54b5
RebeccaZhou666 <55221038+RebeccaZhou666@users.noreply.github.com>
2020-01-14 16:29:30 -0500

    Add files via upload
    Restart function can be used now.

--- startScreen.js
+++ startScreen.js
@@ -17,7 +17,7 @@ function instruction(){
 	if(!created){
 		image(insimg,0,0,width,height);
 		gif = createImg("assets/ins.gif");
-		gif.position(width/2-60, 250);
+		gif.position(width/2-60, 330);
         select('#play').html('Start Game');
 		created = true;
 	}
@@ -41,16 +41,46 @@ function endGame(){
 
 	button_r.mousePressed(function() {
 
-     // clearData();
+     clearData();
 
      initPlay = 1;
      // button_s.position(width/2-100, height/2);
      button_r.style('visibility', 'hidden');
      button.style('visibility', 'visible');
+
+    audioContext = getAudioContext();
+    mic = new p5.AudioIn();
+    mic.start(startPitch);
+    bgm.play();
      // document.getElementById('play').style.visibility='';
 
      // clear pitches
     });
 
+}
+
+function clearData(){
+    pitch = [];
+    audioContext = [];
+    audioStream = [];
+
+    inputPitches = [];
+    currentNote = '';
+    adjust = 0;
+    noteNum = 5;
+
+    perm = [];
+    for (let n = 0; n < 400; n++){
+        perm[n] = 200;
+    }
+
+    t = 0;
+    speed = 0;
+    created = false;
+
+    player = [];
+    player = new Player;
+    your_score = 0;
+    playing = true;
 }
 
